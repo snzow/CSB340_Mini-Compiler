922964b18297b9b4f112068ee036f7a0b1957fe2
snzow <aodhan.bower@gmail.com>
2023-05-16 14:14:38 -0700

    change for git requirement

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -52,7 +52,9 @@ public class Lexer {
 
     static void error(int line, int pos, String msg) {
         if (line > 0 && pos > 0) {
+
             System.out.printf("%s in line %d, pos %d\n", msg, line, pos);
+
         } else {
             System.out.println(msg);
         }
